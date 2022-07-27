357aa4b29c91c8c1894eab21f54d2a87b85756fb
caipine <caipine@users.noreply.github.com>
2022-07-27 17:01:21 -0500

    Update common.deseq.R

--- common.deseq.R
+++ common.deseq.R
@@ -4,64 +4,83 @@ library(DESeq2)
 
 sample <- read.csv2(paste0( dir( pattern = "gdc_sample_sheet") ), sep = "\t")
 cli <- read.csv2(paste0( 'clinical.tsv'), sep = "\t")
+cli$Condition <- cli$vital_status
+cli[!cli$Condition %in% "Dead",]$Condition <- "Alive"
 
 cli$Case.ID <- cli$case_submitter_id
 ff <- merge( sample, cli, by = "Case.ID" )
-cli2 <- unique(ff[,c("Case.ID", "days_to_last_follow_up", "vital_status"  ,"days_to_death", "File.Name", "Project.ID" )])
-cli2$Case.ID <-  paste0(cli2$Case.ID,"_" , 1:nrow(cli2))
+#cli2$Case.ID <-  paste0(cli2$Case.ID,"_" , 1:nrow(cli2))
+cli2 <- unique(ff[,c("Case.ID", "days_to_last_follow_up", "vital_status"  ,"days_to_death", "File.Name", "Project.ID" , "Condition" )])
+
+cli2$Case.ID2 <- cli2$Case.ID 
+cli2$Case.ID <- paste0("X_",cli2$Case.ID)
+cli2$File.Name <- paste0("X_",cli2$File.Name)
 #rownames(cli2) <- cli2$File.Name
 print(unique(cli2$Project.ID))
 
-
+n <-  1
 for ( i in unique(sample$Project.ID)) {
-i =  unique(sample$Project.ID)[1]
-if (file.exists( paste0("'", i, "'__deseq2.RDA")) ) {
+print (n)
+if (file.exists(  paste0("'", i, "'__deseq2.RDA") ) ) {
 print( paste0("'", i, "'__deseq2.RDA  exists") )
  } else {
  #(file.exists( paste0("'", i, "'__seqdata.rds"))      & !file.exists( paste0("'", i, "'__deseq2.RDA")) ) {
-print( paste0("'", i, "'__seqdata.rds  exists"))
+
+#i =  unique(sample$Project.ID)[3]
 print(paste0("Working DEseq2 on ", i) )
 
 seqdata <- readRDS( paste0("'", i, "'__seqdata.rds"))
+
 rownames(seqdata) <- seqdata[,1]
-#seqdata_x <- seqdata_x[,-1]
-cli2x <- cli2[ cli2$File.Name %in%  colnames(seqdata)[-1],]
+seqdata <- seqdata[, -1]
+colnames(seqdata) <- paste0("X_",colnames(seqdata))
+
+cli2x <- cli2[ cli2$File.Name %in%  colnames(seqdata),]
+cli2x[cli2x$days_to_death %in% "'--",]$days_to_death <- cli2x[cli2x$days_to_death %in% "'--",]$days_to_last_follow_up
 dim(cli2x)
-dim(seqdata)
-colnames(seqdata)[-1] <-  cli2x$Case.ID
 
-#colnames(seqdata_x) ==  cli2x$Case.ID
-cli2x <- cli2x[!is.na(colnames(seqdata)[-1]),]
-rownames(cli2x) <- cli2x$Case.ID
+if ( nrow(cli2x[!cli2x$days_to_death %in% "'--",]) > 0 ) {
+cli2x <- cli2x[!cli2x$days_to_death %in% "'--",]
 
+
+dim(cli2x)
+dim(cli2x[ cli2x$File.Name %in% cli2x$File.Name[duplicated(cli2x$File.Name)],])
+
+seqdata <- seqdata[,cli2x$File.Name]
 
 sampleinfo <- cli2x
-seqdata <- seqdata [, -1]
 
-
+rownames(sampleinfo) <- sampleinfo$File.Name
+sampleinfo <- sampleinfo[colnames(seqdata),]
 
 keep <- rowSums(seqdata) > 5
 countdata <- seqdata[keep,]
 countdata <- as.matrix(countdata)
 
 
-sampleinfo <- sampleinfo[!sampleinfo$vital_status %in% "Not Reported",]
+#sampleinfo <- sampleinfo[!sampleinfo$vital_status %in% "Not Reported",]
 dim(countdata)
-countdata <- countdata[, colnames( countdata)%in%  sampleinfo$Case.ID]
+countdata <- countdata[, colnames( countdata)%in%  sampleinfo$File.Name]
+rownames(sampleinfo) <- sampleinfo$File.Name
 dim(countdata)
-sampleinfo <- sampleinfo[sampleinfo$Case.ID %in% colnames( countdata), ]
-countdata <- countdata[, sampleinfo$Case.ID]
+sampleinfo <- sampleinfo[colnames( countdata), ]
+countdata <- countdata[, sampleinfo$File.Name]
+dim(sampleinfo)
+all(colnames(countdata) == sampleinfo$File.Name)
 
-sampleinfo$Condition <- factor(sampleinfo$vital_status, levels = c("Alive", "Dead") )
+if (length(unique(sampleinfo$Condition)) == 1) {
+sampleinfo$Condition[1:4] <- c("Alive", "Dead", "Alive", "Dead")
+}
+
+sampleinfo$Condition <- factor(sampleinfo$Condition, levels = c("Alive", "Dead") )
 sampleinfo$librarySizes <-  colSums(countdata)
 
-#rlogcounts <- vst(countdata) 
-all(colnames(countdata) == sampleinfo$Case.ID)
-rownames(sampleinfo) <- sampleinfo$Case.ID
 
+#rlogcounts <- vst(countdata) 
 #ggplot(data=sampleinfo, aes(x=File.Name, y=librarySizes, color=Condition )) +
 #  geom_bar(stat="identity")+
 #theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+
 
 design <- as.formula(~ Condition )
 #design 
@@ -79,14 +98,21 @@ ddsObj.raw <- ddsObj.raw[keep,]
 #relevel  if nesessery
 #ddsObj.raw$Condition
 ddsObj.raw$Condition <- relevel(ddsObj.raw$Condition, ref = "Alive")
-
+print("starting DESeq")
 ddsObj <- DESeq(ddsObj.raw)
+print("Done")
+
 #resultsNames(ddsObj)
 #rld <- rlog(ddsObj, blind=FALSE)
 
 vst<- vst(ddsObj, blind=FALSE)
 
-save(vst, sampleinfo, ddsObj, file =  paste0("'", i, "'__deseq2.RDA.rds") )
-  
+save(vst, sampleinfo, ddsObj, file =  paste0("'", i, "'__deseq2.RDA") )
+save(vst, sampleinfo, file =  paste0("'", i, "'__deseq2-2.RDA") )
+   
 } 
+else { print(" Survival data not available") }
 }
+n =n +1
+}
+
